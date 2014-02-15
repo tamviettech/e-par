433dffee39399a928636c0b3cfa5b10789da34c0
Tam Viet Technology <congnghetamviet@gmail.com>
2014-02-15 16:02:44 +0700

    Nâng cấp hỗ trợ thu phí
    + Thêm checkbox đánh dấu "Đã thu đủ" tại màn hình tiếp nhận
        + Nếu
    "Đã thu đủ": Lệ phí là phí đã tạm thu ("không lấy fee khai báo trong
    workflow)
        + Phí tạm thu là bắt buộc phải nhập

--- public_html/apps/r3/modules/record/record_views/dsp_charging_record.php
+++ public_html/apps/r3/modules/record/record_views/dsp_charging_record.php
@@ -1,23 +1,4 @@
 <?php
-/**
-
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-?>
-
-<?php
 if (!defined('SERVER_ROOT'))
     exit('No direct script access allowed');
 $arr_single_record = $VIEW_DATA['arr_single_record'];
@@ -27,6 +8,7 @@ $v_record_type_code = $arr_single_record['C_RECORD_TYPE_CODE'];
 $v_record_type_name = $arr_single_record['C_RECORD_TYPE_NAME'];
 $v_citizen_name     = $arr_single_record['C_CITIZEN_NAME'];
 $v_record_no        = $arr_single_record['C_RECORD_NO'];
+$v_xml_data         = $arr_single_record['C_XML_DATA'];
 
 $arr_all_next_user = $VIEW_DATA['arr_all_next_user'];
 
@@ -35,6 +17,7 @@ $this->template->title = 'Thu phí, lệ phí';
 
 $v_pop_win = isset($_REQUEST['pop_win']) ? '_pop_win' : '';
 $this->template->display('dsp_header' . $v_pop_win . '.php');
+
 
 //Da tam thu
 $v_advance_cost = $arr_single_record['C_ADVANCE_COST'];
@@ -59,6 +42,15 @@ $v_cost_desc    = implode("\n", $arr_cost_desc);
 $v_xml_workflow = $this->get_xml_config($v_record_type_code, 'workflow');
 $dom_flow       = simplexml_load_file($v_xml_workflow);
 $v_default_fee  = xpath($dom_flow, "//process/@fee", XPATH_STRING);
+
+//LienND update 2014-02-15: Kiem tra "Da thu du" chua?
+$dom_xml_data = simplexml_load_string($v_xml_data);
+$v_advance_cost_is_full = get_xml_value($dom_xml_data, '//item[@id="chk_txtCost_full"]/value');
+//Neu da thu du, khong lay fee trong workflow
+if (strtolower($v_advance_cost_is_full) == 'true')
+{
+    $v_default_fee = $v_advance_cost;
+}
 ?>
 <form name="frmMain" method="post" id="frmMain" action="<?php echo $this->get_controller_url(); ?>do_charging_record">
     <?php
@@ -77,98 +69,109 @@ $v_default_fee  = xpath($dom_flow, "//process/@fee", XPATH_STRING);
 //Phi da tam thu
     echo $this->hidden('hdn_advance_cost', $v_advance_cost);
     ?>
-    <div class="Row">
-        <div class="left-Col">Loại hồ sơ:</div>
-        <div class="right-Col"><?php echo $v_record_type_code; ?> - <?php echo $v_record_type_name; ?></div>
+    <div class="widget-head blue">
+        <h3>Thu phí</h3>
     </div>
-    <div class="Row">
-        <div class="left-Col">Tên người đăng ký</div>
-        <div class="right-Col"><?php echo $v_citizen_name; ?></div>
-    </div>
-    <div class="Row">
-        <div class="left-Col">Mã hồ sơ</div>
-        <div class="right-Col"><?php echo $v_record_no; ?></div>
-    </div>
-    <div class="Row">
-        <div class="left-Col">Lệ phí</div>
-        <div class="right-Col">
-            <input type="text" name="txt_fee" id="txt_fee"
-                   size="8" maxlength="10" class="text ui-widget-content ui-corner-all"
-                   value="<?php echo number_format($v_default_fee); ?>"
-                   onchange="txt_fee_onkeyup()"
-                   /> (đ)
-            <label id="lbl_text_fee"></label>
-        </div>
-    </div>
-    <div class="Row">
-        <div class="left-Col">Phí thụ lý</div>
-        <div class="right-Col">
-            <input type="text" name="txt_cost" id="txt_cost" 
-                   size="8" maxlength="10" class="text ui-widget-content ui-corner-all"
-                   value="<?php echo $v_cost; ?>" readonly
-                   onchange="txt_cost_onkeyup()"
-                   /> (đ)
-            <label id="lbl_text_cost"></label>
-        </div>
-    </div>
-    <div class="Row">
-        <div class="left-Col">Đã tạm thu</div>
-        <div class="right-Col">
-            <input type="text" name="txt_avanced_cost" id="txt_avanced_cost" readonly
-                   size="8" maxlength="10" class="text ui-widget-content ui-corner-all"
-                   value="<?php echo number_format($v_advance_cost); ?>" readonly="1"/> (đ)
-            <label id="lbl_text_advanced_cost"></label>
-        </div>
-
-    </div>
-    <div class="Row">
-        <div class="left-Col" id="additional_fee_label" ></div>
-        <div class="right-Col" >
-            <input type="text" name="additional_fee" id="additional_fee" style="color:purple"
-                   size="8" maxlength="10" class="text ui-widget-content ui-corner-all"
-                   readonly="1"/> (đ)
-            <label id="lbl_text_additional_fee"></label>
-
-        </div>
-    </div>
-    <div class="Row">
-        <div class="left-Col">Diễn giải</div>
-        <div class="right-Col">
-            <textarea style="width:100%;height:100px;" rows="1"
-                      name="txt_fee_description" id="txt_fee_description" cols="20" maxlength="400"
-                      class="text ui-widget-content ui-corner-all"><?php echo $v_cost_desc; ?></textarea>
-        </div>
-    </div>
-    <div class="Row">
-        <div id="next_user">
+    <table style="width: 100%;" class="none-border-table">
+        <tr>
+            <td style="font-weight: bold" width="15%">Loại hồ sơ:</td>
+            <td><?php echo $v_record_type_code; ?> - <?php echo $v_record_type_name; ?></td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold" width="15%">Tên người đăng ký</td>
+            <td><?php echo $v_citizen_name; ?></td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold" width="15%">Mã hồ sơ</td>
+            <td><?php echo $v_record_no; ?></td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold" width="15%">Lệ phí</td>
+            <td>
+                <input type="text" name="txt_fee" id="txt_fee"
+                       size="8" maxlength="10" class="text valid"
+                       value="<?php echo number_format($v_default_fee); ?>"
+                       onchange="txt_fee_onkeyup()"
+                       /> (đ)
+                <label id="lbl_text_fee"></label>
+            </td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold" width="15%">Phí thụ lý</td>
+            <td>
+                <input type="text" name="txt_cost" id="txt_cost" 
+                       size="8" maxlength="10" class="text valid"
+                       value="<?php echo $v_cost; ?>" readonly
+                       onchange="txt_cost_onkeyup()"
+                       /> (đ)
+                <label id="lbl_text_cost"></label>
+            </td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold" width="15%">Đã tạm thu</td>
+            <td>
+                <input type="text" name="txt_avanced_cost" id="txt_avanced_cost" readonly
+                       size="8" maxlength="10" class="text valid"
+                       value="<?php echo number_format($v_advance_cost); ?>" readonly="1"/> (đ)
+                <label id="lbl_text_advanced_cost"></label>
+            </td>
+    
+        </tr>
+        <tr>
+            <td style="font-weight: bold" width="15%" id="additional_fee_label" ></td>
+            <td >
+                <input type="text" name="additional_fee" id="additional_fee" style="color:purple"
+                       size="8" maxlength="10" class="text valid"
+                       readonly="1"/> (đ)
+                <label id="lbl_text_additional_fee"></label>
+    
+            </td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold" width="15%">Diễn giải</td>
+            <td>
+                <textarea style="width:100%;height:100px;" rows="1"
+                          name="txt_fee_description" id="txt_fee_description" cols="20" maxlength="400"
+                          class="text valid"><?php echo $v_cost_desc; ?></textarea>
+            </td>
+        </tr>
+        <tr>
+            
             <?php if (count($arr_all_next_user) > 0): ?>
-                <div class="Row">
-                    <div class="left-Col">Cán bộ <?php echo $this->role_text[get_role($arr_all_next_user[0]['C_TASK_CODE'])]; ?></div>
-                    <div class="right-Col">
-                        <ul id="signer">
+                    <td style="font-weight: bold" width="15%">Cán bộ <?php echo $this->role_text[get_role($arr_all_next_user[0]['C_TASK_CODE'])]; ?></td>
+                    <td>
+                        <div class="control-group">
+                            <div class="controls">
                             <?php for ($i = 0; $i < count($arr_all_next_user); $i++): ?>
                                 <?php if ($arr_all_next_user[$i]['FK_OU'] == Session::get('ou_id') OR (!Session::get('la_can_bo_cap_xa'))): ?>
-                                    <li>
-                                        <input type="radio" value="<?php echo $arr_all_next_user[$i]['C_USER_LOGIN_NAME']; ?>"
-                                               id="rad_next_user_<?php echo $i; ?>" name="rad_next_user"
-                                               <?php echo ($i == 0 OR $arr_all_next_user[$i]['FK_OU'] == Session::get('ou_id')) ? ' checked' : ''; ?> />
                                         <label for="rad_next_user_<?php echo $i; ?>">
-                                            <?php echo $arr_all_next_user[$i]['C_NAME']; ?> <i>(<?php echo $arr_all_next_user[$i]['C_JOB_TITLE']; ?>)</i>
+                                            
+                                            <input type="radio" value="<?php echo $arr_all_next_user[$i]['C_USER_LOGIN_NAME']; ?>"
+                                           id="rad_next_user_<?php echo $i; ?>" name="rad_next_user"
+                                           <?php echo ($i == 0 OR $arr_all_next_user[$i]['FK_OU'] == Session::get('ou_id')) ? ' checked' : ''; ?> />
+                                           <?php echo $arr_all_next_user[$i]['C_NAME']; ?> <i>(<?php echo $arr_all_next_user[$i]['C_JOB_TITLE']; ?>)</i>
                                         </label>
-                                    </li>
                                 <?php endif; ?>
                             <?php endfor; ?>
-                        </ul>
-                    </div>
-                </div>
-            <?php endif; ?>
-        </div>
+                            </div>
+                        </div>
+                    </td>
+                <?php endif; ?>
+            
+            </tr>
+        </table>
         <!-- Buttons -->
         <div class="button-area">
             <hr/>
-            <input type="button" name="btn_do_exec" class="button save" value="Thu phí" onclick="btn_do_charging_onclick();" />
+            <button type="button" name="btn_do_exec" class="btn btn-primary" onclick="btn_do_charging_onclick();" accesskey="2">
+                <i class="icon-save"></i>
+                <?php echo __('Thu phí'); ?>
+            </button>
             <?php $v_back_action = ($v_pop_win === '') ? 'btn_back_onclick();' : 'try{window.parent.hidePopWin();}catch(e){window.close();};'; ?>
-            <input type="button" name="cancel" class="button close" value="<?php echo __('close window'); ?>" onclick="<?php echo $v_back_action; ?>"/>
+            <button type="button" name="cancel" class="btn btn-danger" onclick="<?php echo $v_back_action;?>" >
+                <i class="icon-remove"></i>
+                <?php echo __('close window'); ?>
+            </button> 
         </div>
 </form>
 <script>
