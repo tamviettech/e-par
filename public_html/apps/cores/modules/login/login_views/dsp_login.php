c0e247bb53bb1a5ba9b0cc357352e22395c5578f
Tam Viet Technology <congnghetamviet@gmail.com>
2014-04-29 14:37:57 +0700

    Fix mã hoá mật khẩu trước khi submit: Bổ sung dòng lệnh xoá mật khẩu đã nhập, chỉ submit mật khẩu đã mã hoá

--- public_html/apps/cores/modules/login/login_views/dsp_login.php
+++ public_html/apps/cores/modules/login/login_views/dsp_login.php
@@ -47,6 +47,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                 }
                 
                 document.loginForm.hdn_password_md5.value = CryptoJS.MD5(document.loginForm.txt_password.value);
+                document.loginForm.txt_password.value = "";
                 f.submit();
             }
 
